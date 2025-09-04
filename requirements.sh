5c57c5e9fd2b6e5c34b0e7befae6e798b1c19313
helloworld <30624620+spdj2271@users.noreply.github.com>
2025-09-04 14:10:17 +0800

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,5 @@
-conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
+pip install lightning==2.0.9
+pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
 pip install numpy==1.21.0 scikit-learn==1.2.2 scipy==1.10.1 \
 tensorboard==2.13.0 Pillow==9.5.0 termcolor==2.3.0 PyYAML==6.0 easydict==1.10
 pip install lightly==1.4.18
